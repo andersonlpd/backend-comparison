46714078d11e923c6f9db8b479ede22d76ae4627
Anderson Dorigão <andersonlpd@gmail.com>
2025-06-05 22:00:25 -0300

    Removendo a versão em Django

--- python-backend/app/users_uc/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersUcConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users_uc'
--- python-backend/app/users_uc/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView, RegisterUserView
-
-urlpatterns = [
-    path('users/', UserListCreateAPIView.as_view()),
-    path('users/<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view()),
-    path('register/', RegisterUserView.as_view(), name='register'),
-]
